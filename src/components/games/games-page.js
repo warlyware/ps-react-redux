3ce323f316d0ad4e96fbc85f44ac7c43e06ee48a
warlyware <danward@gmail.com>
2017-09-23 12:15:12 -0400

    Add Games page, add routing

---
+++ src/components/games/games-page.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+class GamesPage extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Games</h1>
+      </div>
+    );
+  };
+}
+
+export default GamesPage;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
-import HomePage from './components/home/home';
-import AboutPage from './components/about/about';
+import HomePage from './components/home/home-page';
+import AboutPage from './components/about/about-page';
+import GamesPage from './components/games/games-page'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
+    <Route path="games" component={GamesPage} />
   </Route>
 );
