718e7d9e7c668441cb46f4944b33bbec6d5c36fd
Dan Ward <daniel.ward@forrent.com>
2017-10-10 16:20:45 -0400

    Add thunk middleware

--- src/components/games/games-page.js
+++ src/components/games/games-page.js
@@ -6,27 +6,6 @@ import * as gameActions from '../actions/game-actions';
 class GamesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      game: {
-        title: ''
-      }
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleTitleChange = this.handleTitleChange.bind(this);
-  }
-
-  handleTitleChange(event) {
-    const game = this.state.game;
-    game.title = event.target.value;
-    this.setState({
-      game: game
-    });
-  }
-
-  handleSubmit() {
-    this.props.actions.createGame(this.state.game);
   }
 
   gameRow(game, index) {
@@ -38,13 +17,6 @@ class GamesPage extends React.Component {
       <div>
         <h1>Games</h1>
         {this.props.games.map(this.gameRow)}
-        <h3>Add Game</h3>
-        <input type="text"
-        onChange={this.handleTitleChange}
-        value={this.state.game.title} />
-        <input type="submit"
-        onClick={this.handleSubmit}
-        value="save"/>
       </div>
     );
   }
